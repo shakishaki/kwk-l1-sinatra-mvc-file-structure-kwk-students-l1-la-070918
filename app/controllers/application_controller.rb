87d43e85b71ef43dfd3ce46a63c606130c2f50e0
shakishaki <shaki.canchola@yahoo.com>
2018-07-17 22:41:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  
+  post "/" do
+    @the_fortune=get_fortune
+    @the_user=params[:user]
+    return erb :results
+  end
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
-  # Replace with Dog clas
+
+  # THIS IS WHERE THE RUBY HAPPENS
+def get_fortune
+  fortunes_array = ["You've already met your soulmate and you let them slip away.", "Do not go hiking. You will fall down the mountain.", "Don't get into the red car. You will never get back out.", "Beware dogs off leash.", "You will spend your 45th birthday alone.", "Don't go out next Fourth of July", "Your hardwork will pay off in the very far future.", "Your determination will take you far."]
+  return fortunes_array.sample
 end
+
+puts get_fortune
+  
